e749ecefe5d7f99032404d26684c64dfbceafe20
manandharnitesh84@gmail.com <manandharnitesh84@gmail.com>
2021-06-26 12:40:29 +0545

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,14 +3,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[Controller]")]
-    public class UsersController : ControllerBase
+    // [ApiController]
+    // [Route("api/[Controller]")]
+    //not needed as attribute are inherited from BaseApiController
+    public class UsersController : BaseApiController  //ControllerBase
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -19,40 +21,44 @@ namespace API.Controllers
 
         }
 
-      /*
+        /*
 
-      Synchronus
+        Synchronus
+          [HttpGet]
+          public ActionResult<IEnumerable<AppUser>> GetUsers(){
+              // public ActionResult<List<AppUser>> GetUsers(){
+              return   _context.Users.ToList();
+
+          }
+
+    //api/users/3
+           [HttpGet("{id}")]
+          public ActionResult<AppUser> GetUser(int id){
+              // public ActionResult<List<AppUser>> GetUsers(){
+                  return   _context.Users.Find(id);//Finds Primary key
+
+
+          }
+  */
+        //Asynchronus
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers(){
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
             // public ActionResult<List<AppUser>> GetUsers(){
-            return   _context.Users.ToList();
-          
-        }
+            return await _context.Users.ToListAsync();
 
-  //api/users/3
-         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id){
-            // public ActionResult<List<AppUser>> GetUsers(){
-                return   _context.Users.Find(id);//Finds Primary key
-     
-
-        }
-*/
-//Asynchronus
-[HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-            // public ActionResult<List<AppUser>> GetUsers(){
-           return await _context.Users.ToListAsync();
-      
 
         }
 
-  //api/users/3
-         [HttpGet("{id}")]
-        public async Task< ActionResult<AppUser> >GetUser(int id){
+        //api/users/3
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
             // public ActionResult<List<AppUser>> GetUsers(){
-                return await  _context.Users.FindAsync(id);//Finds Primary key
-     
+            return await _context.Users.FindAsync(id);//Finds Primary key
+
 
         }
 
